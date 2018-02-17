90748695468a1fcb0166e0b0841ccb9786c5a30c
= <=>
2018-02-17 16:05:45 -0500

    close #4 flickering issue

--- web/about.php
+++ web/about.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
   <link rel="icon" type="image/png" href="favicon.png"></link>
-  <title>HomeWorks | About</title>
+  <title>About | HomeWorks</title>
   <!-- Google font -->
   <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700%7CVarela+Round" rel="stylesheet">
   <!-- Bootstrap -->
@@ -14,74 +14,63 @@
   <!-- Owl Carousel -->
   <link type="text/css" rel="stylesheet" href="css/owl.carousel.css" />
   <link type="text/css" rel="stylesheet" href="css/owl.theme.default.css" />
-  <!-- Magnific Popup -->
-  <link type="text/css" rel="stylesheet" href="css/magnific-popup.css" />
   <!-- Font Awesome Icon -->
   <link rel="stylesheet" href="css/font-awesome.min.css">
   <!-- Custom stlylesheet -->
   <link type="text/css" rel="stylesheet" href="css/style.css" />
-  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-  <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-  <![endif]-->
 </head>
 <!-- /Head -->
 <body>
 <!-- Nav -->
-<div id='nav-div'>
-  <!-- <script>$(function(){$('#nav-div').load('nav.php')});</script> -->
-  <nav id="nav" class="navbar nav-transparent">
-    <div class="container">
-      <div class="navbar-header">
-        <!-- Logo -->
-        <div class="navbar-brand">
-          <a href="index.php">
-            <img class="logo" src="img/logo-top.png" alt="HomeWorks">
-            <img class="logo-alt" src="img/logo-top.png" alt="HomeWorks">
-          </a>
-        </div>
-        <!-- /Logo -->
-        <!-- Collapse nav button -->
-        <div class="nav-collapse">
-          <span></span>
-        </div>
-        <!-- /Collapse nav button -->
+<nav id="nav" class="navbar nav-transparent">
+  <div class="container">
+    <div class="navbar-header">
+      <!-- Logo -->
+      <div class="navbar-brand">
+        <a href="index.php">
+          <img class="logo" src="img/logo-top.png" alt="HomeWorks">
+          <img class="logo-alt" src="img/logo-top.png" alt="HomeWorks">
+        </a>
       </div>
-      <!--  Main navigation  -->
-      <ul id="nav-list" class="main-nav nav navbar-nav navbar-right">
-        <!-- JavaScript Generated Menu -->
-        <li id="nav-Home">
-          <a href="index.php">Home</a>
-        </li>
-        <li id="nav-About">
-          <a href="about.php">About</a>
-        </li>
-        <li id="nav-Portfolio" class="has-dropdown">
-          <a href="portfolio.php">Portfolio</a>
-          <ul class="dropdown">
-            <li id="nav-Bathrooms" class="open-drop">
-              <a href="portfolio.php#bathrooms">Bathrooms</a>
-            </li>
-            <li id="nav-Living Rooms" class="open-drop">
-              <a href="portfolio.php#living-rooms">Living Rooms</a>
-            </li>
-          </ul>
-        </li>
-        <li id="nav-Testimonials">
-          <a href="testimonials.php">Testimonials</a>
-        </li>
-        <li id="nav-Links">
-          <a href="links.php">Links</a>
-        </li>
-        <li id="nav-Contact">
-          <a href="contact.php">Contact</a>
-        </li></ul>
-      <!-- /Main navigation -->
+      <!-- /Logo -->
+      <!-- Collapse nav button -->
+      <div class="nav-collapse">
+        <span></span>
+      </div>
+      <!-- /Collapse nav button -->
     </div>
-  </nav>
-</div>
+    <!--  Main navigation  -->
+    <ul id="nav-list" class="main-nav nav navbar-nav navbar-right">
+      <!-- JavaScript Generated Menu -->
+      <li id="nav-Home">
+        <a href="index.php">Home</a>
+      </li>
+      <li id="nav-About">
+        <a href="about.php">About</a>
+      </li>
+      <li id="nav-Portfolio" class="has-dropdown">
+        <a href="portfolio.php">Portfolio</a>
+        <ul class="dropdown">
+          <li id="nav-Bathrooms" class="open-drop">
+            <a href="portfolio.php#bathrooms">Bathrooms</a>
+          </li>
+          <li id="nav-Living Rooms" class="open-drop">
+            <a href="portfolio.php#living-rooms">Living Rooms</a>
+          </li>
+        </ul>
+      </li>
+      <li id="nav-Testimonials">
+        <a href="testimonials.php">Testimonials</a>
+      </li>
+      <li id="nav-Links">
+        <a href="links.php">Links</a>
+      </li>
+      <li id="nav-Contact">
+        <a href="contact.php">Contact</a>
+      </li></ul>
+    <!-- /Main navigation -->
+  </div>
+</nav>
 <!-- /Nav -->
 <!-- About -->
 <div id="about" class="section md-padding">
@@ -182,7 +171,6 @@
   <script type="text/javascript" src="js/jquery.min.js"></script>
   <script type="text/javascript" src="js/bootstrap.min.js"></script>
   <script type="text/javascript" src="js/owl.carousel.min.js"></script>
-  <script type="text/javascript" src="js/jquery.magnific-popup.js"></script>
   <script type="text/javascript" src="js/main.js"></script>
   <!-- /jQuery Plugins -->
 </div>
--- web/js/main.js
+++ web/js/main.js
@@ -2,35 +2,6 @@
 	"use strict"
 
 	///////////////////////////
-	// Preloader
-	$(window).on('load', function() {
-		$("#preloader").delay(600).fadeOut();
-	});
-
-	///////////////////////////
-	// Scrollspy
-	$('body').scrollspy({
-		target: '#nav',
-		offset: $(window).height() / 2
-	});
-
-	///////////////////////////
-	// Smooth scroll
-	$("#nav .main-nav a[href^='#']").on('click', function(e) {
-		e.preventDefault();
-		var hash = this.hash;
-		$('html, body').animate({
-			scrollTop: $(this.hash).offset().top
-		}, 600);
-	});
-
-	$('#back-to-top').on('click', function(){
-		$('body,html').animate({
-			scrollTop: 0
-		}, 600);
-	});
-
-	///////////////////////////
 	// Btn nav collapse
 	$('#nav .nav-collapse').on('click', function() {
 		$('#nav').toggleClass('open');
@@ -49,19 +20,7 @@
 	// On Scroll
 	$(window).on('scroll', function() {
 		var wScroll = $(this).scrollTop();
-
-		// Fixed nav
 		wScroll > 1 ? $('#nav').addClass('fixed-nav') : $('#nav').removeClass('fixed-nav');
-
-		// Back To Top Appear
-		// wScroll > 700 ? $('#back-to-top').fadeIn() : $('#back-to-top').fadeOut();
-	});
-
-	///////////////////////////
-	// magnificPopup
-	$('.work').magnificPopup({
-		delegate: '.lightbox',
-		type: 'image'
 	});
 
 })(jQuery);
