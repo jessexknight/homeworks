524272adc89f3fa3b986b892d542bcb541cf0839
= <=>
2018-02-18 12:34:49 -0500

    close #2 mobile portfolio clicks

--- web/js/main.js
+++ web/js/main.js
@@ -8,13 +8,24 @@
 	});
 
 	///////////////////////////
-	// Mobile dropdown
+	// Mobile dropdown click
   $('.has-dropdown').children('a').on('click', function(e) {
 		$(this).parent().toggleClass('open-drop');
     if ($(this).parent().hasClass('open-drop') && $(window).width() < 767) {
       e.preventDefault();
     };
 	});
+
+  ///////////////////////////
+	// Mobile portfolio click
+  $(window).load(function() {
+    $('.work').children('a').on('click', function(e) {
+  		$(this).toggleClass('mobile-click');
+      if ($(this).hasClass('mobile-click') && $(window).width() < 767) {
+        e.preventDefault();
+      };
+  	});
+  });
 
 	///////////////////////////
 	// On Scroll
