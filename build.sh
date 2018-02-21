c64f052c28fec549391737e41165b2b9108d6f09
= <=>
2018-02-20 23:47:36 -0500

    testing

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ gdrive download -r -f 1E15Njd_eV2HS8Uv6pdTiCEJX88TKus7X
 echo "Building Webpages ..."
 cd ~/dev/web/homeworks/main/src
 python build.py
+cd ~/dev/web/homeworks/main/
 git add .
 git commit -m "${1:-automated-build}"
-git push master
 git push heroku master
