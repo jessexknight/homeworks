e2a8e4f2c2014cf4413ce44ba33bc828b77dbb55
= <=>
2018-02-23 12:14:20 -0500

    finalize build script

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-cwd="${BASH_SOURCE[0]}"
-POSITIONAL=()
+cwd="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 while [[ $# -gt 0 ]]
 do
 key="$1"
@@ -9,20 +8,20 @@ key="$1"
 case $key in
     d)
     echo "Downloading Portfolio ..."
-    cd $(pwd)/web
+    cd $cwd/web
     rm -rf portfolio
     gdrive download -r -f 1E15Njd_eV2HS8Uv6pdTiCEJX88TKus7X
     shift
     ;;
     b)
     echo "Building Webpages ..."
-    cd $(pwd)/src
+    cd $cwd/src
     python build.py
     shift
     ;;
     p)
     echo "Pushing to Heroku ..."
-    cd $(pwd)
+    cd $cwd
     git add .
     git commit -m "automated-build"
     git push master
