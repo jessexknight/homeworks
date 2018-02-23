851a180be64aac0857c512203abe3c7817035fb1
= <=>
2018-02-23 12:04:07 -0500

    automated-build

--- build.sh
+++ build.sh
@@ -1,16 +1,6 @@
-# echo "Building Webpages ..."
-# cd ~/dev/web/homeworks/main/src
-# python build.py
-# cd ~/dev/web/homeworks/main/
-# git add .
-# git commit -m "${1:-automated-build}"
-# git push master
-# git push heroku master
-
-
 #!/bin/bash
 
-cwd=$(pwd)
+cwd="${BASH_SOURCE[0]}"
 POSITIONAL=()
 while [[ $# -gt 0 ]]
 do
@@ -19,18 +9,18 @@ key="$1"
 case $key in
     d)
     echo "Downloading Portfolio ..."
-    cd $PATH:$(pwd)+web
+    cd $(pwd)/web
     rm -rf portfolio
     gdrive download -r -f 1E15Njd_eV2HS8Uv6pdTiCEJX88TKus7X
     shift;;
     b)
     echo "Building Webpages ..."
-    cd $PATH:$(pwd)+src
+    cd $(pwd)/src
     python build.py
     shift;;
     p|--msg)
     echo "Pushing to Heroku ..."
-    cd ~/dev/web/homeworks/main/
+    cd $(pwd)
     git add .
     git commit -m "${2:-automated-build}"
     git push master
